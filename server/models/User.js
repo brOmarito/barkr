d1702d2b520015078bac1b9a6effbbf4f13f1161
Josh Elrod <elrodjosh96@gmail.com>
2022-02-05 11:50:01 -0500

    added mongo stuff, added basic model code, updates on resolvers/typeDefs

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = model('User', userSchema);
+
+const {profileSchema} = require('./Profile');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    }, 
+    savedProfile: {profileSchema},
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+
+module.exports = User;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,36 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedProfile: [Profile]
+}
+type Profile {
+    bio: String!
+    description: String!
+    profileId: String!
+    image: String
+    link: String
+}
+type Auth {
+    token: String
+    user: [User]
+}
+type: Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveProfile(profileToSave: ProfileInput): User 
+        removeProfile(profileId: String!): User
+      }
+  }
 `;
 
 module.exports = typeDefs;
