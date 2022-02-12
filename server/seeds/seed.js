a1160b9c135620b019ff06a0f95f83990cf05b64
Josh Elrod <elrodjosh96@gmail.com>
2022-02-12 14:44:33 -0500

    added seeds, works in mongo

---
+++ server/seeds/seed.js
@@ -0,0 +1,67 @@
+// const db = require('../config/connection');
+const { User } = require('../models');
+
+const userData = [
+    {
+        firstName: 'Frank',
+        lastName: 'Smith',
+        username: 'Fsmith456',
+        email: 'frank@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Elijah',
+        lastName: 'Wood',
+        username: 'eWood99',
+        email: 'ewood@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Billy',
+        lastName: 'Bob',
+        username: 'billyboy489',
+        email: 'bbob@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Eric',
+        lastName: 'Taylor',
+        username: 'eTaylor',
+        email: 'bbob@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Johnny',
+        lastName: 'Bravo',
+        username: 'johnnyB56',
+        email: 'jBravo@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Luke',
+        lastName: 'Skywalker',
+        username: 'Lskydaddy',
+        email: 'Lwalker@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Chris',
+        lastName: 'Brown',
+        username: 'Cbrown852',
+        email: 'Cbrown@testmail.com',
+        password: 'password12345'
+    },
+    {
+        firstName: 'Kirby',
+        lastName: 'Smart',
+        username: 'GoDawgs3318',
+        email: 'Ksmart@testmail.com',
+        password: 'password12345'
+    },
+];
+console.log('users seeded')
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
+
