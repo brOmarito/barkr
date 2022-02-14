ae7aa71c1f5b4d08ffd4711d955c433dbd395b76
Jacob McCarthy <thejacobmccarthy@gmail.com>
2022-02-14 18:06:13 -0500

    query fix to include image

--- client/src/pages/Dashboard/SideNav/SideNav.jsx
+++ client/src/pages/Dashboard/SideNav/SideNav.jsx
@@ -58,12 +58,14 @@ const NavItem = ({ icon, to, children, ...rest }) => {
 const SideNav = (props) => {
   const { city, dogName, state, dogDescription, image} = props.userInfo
 
+  console.log(props.userInfo)
+
   return (
     <Flex flex='1' justifyContent='center' pt={4} borderRight="1px" borderRightColor="#EDF2F7">
       <VStack>
         <VStack minW="100%" mb={3}>
           <Box display='flex' mb={3} flex='1' flexDirection="column" alignContent="center" justifyContent="center">
-            <Image maxW="150px" src='https://res.cloudinary.com/dkmlyifpy/image/upload/v1644773751/photo-1644187689076-37b6126afada_vwajd2.jpg' objectFit="contain" borderRadius="full" alt='Dog'/>
+            <Image maxW="150px" src={image} objectFit="contain" borderRadius="full" alt='Dog'/>
           </Box>
           <Heading size='md' align="center" maxW="60%">Hello, {dogName}! </Heading>
           <Text noOfLines={3} align="center" fontSize='xl' maxW="60%">{dogDescription} </Text>
