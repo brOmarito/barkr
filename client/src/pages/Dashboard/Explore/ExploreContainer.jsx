3d65cccfc327e26f7fb39cf348bda708f71bc191
SeanOVeryMac <seanovery1@gmail.com>
2022-02-13 21:28:25 -0500

    fixed explore container so it actually loads on first load

--- client/src/pages/Dashboard/Explore/ExploreContainer.jsx
+++ client/src/pages/Dashboard/Explore/ExploreContainer.jsx
@@ -6,20 +6,23 @@ import {
 import { useQuery } from '@apollo/client';
 import { QUERY_PROFILES } from '../../../utils/queries';
 import ProfileCard from './ProfileCard';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import Auth from "../../../utils/auth"
 
-const ExploreContainer = ({ clickHandler, currentProfile }) => {
+const ExploreContainer =  ({ clickHandler, currentProfile }) => {
   const {loading, data} = useQuery(QUERY_PROFILES);
   const [profiles, setProfiles] = useState([]);
-
+  console.log(Auth.getProfile().data._id)
   // Get all profiles and filter out current user
   // TODO: Add a message for no users found
+ 
   useEffect(() => {
-    if (loading) return null
+    if (loading === false && data) {
     let profiles = data.profiles;
     let  filtered = profiles.filter(profile => profile.userId !== currentProfile.userId);
     setProfiles(filtered)
-  })
+    }
+  },[loading, data])
 
   return (
     <Flex flex='4' flexDirection='column' alignItems='center' mt={2}>
